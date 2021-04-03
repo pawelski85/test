7f85df3a30d6c190c0ae1aaf634b52dae3d0a1e0
Pawel <pawel.tomczyk85@gmail.com>
2021-04-03 16:50:42 +0200

    beta

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,10 +4,7 @@ public class Main {
 
     public static void main(String[] args) {
 
-        // beta
-        // beta
-        // beta
-        // beta
+        // tylko beta
 
         // gamma
         // gamma
