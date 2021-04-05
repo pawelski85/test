7b5963ad83c5846750979803456ef346b7374ac5
Pawel <55628933+pawelski85@users.noreply.github.com>
2021-04-05 20:59:48 +0200

    Beta (#56)
    * master
    
    * beta
    
    * beta
    
    * master
    
    Co-authored-by: charlotte-dot <k.smolinska90@gmail.com>

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,11 +3,11 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
+        //master
+        //master
         //beta
         //beta
         //beta
         //beta//beta
-
-
     }
 }
