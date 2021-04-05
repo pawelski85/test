b99c455b5c299ac015c9da0cb43cfdc26b8de785
Pawel <55628933+pawelski85@users.noreply.github.com>
2021-04-05 21:05:23 +0200

    rebase (#58)

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,11 +3,8 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-        //master
-        //master
-        //beta
-        //beta
-        //beta
-        //beta//beta
+
+        //gamma
+
     }
 }
