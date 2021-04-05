41c07aa8a37cbb3e9a0ef9821975ad2c7fec862a
charlotte-dot <k.smolinska90@gmail.com>
2021-04-05 18:20:51 +0200

    master

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,23 +3,6 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-        //beta
-        //aa
-        //aa
-//cc
-
-        //2
-
-        // beta// beta// beta// beta
-// beta// beta// beta// beta// beta
-// beta// beta// beta// beta// beta
-        // gamma
-        // gamma
-        // gamma
-
-        // gamma
-        // gamma
-
-
+        //master
     }
 }
